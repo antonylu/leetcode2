5c494db5d27f60478e1079c719479bd47dcbe9ec
Antony Lu <antony_lu@compal.com>
2018-04-25 15:57:00 +0800

    021 brute force, iterative, compare nodes in two list one by one and append to the new list

---
+++ Python/021_Merge_Two_Sorted_Lists.py
@@ -0,0 +1,61 @@
+# https://leetcode.com/problems/merge-two-sorted-lists/description/
+# 
+# Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+# 
+# Example:
+# 
+# Input: 1->2->4, 1->3->4
+# Output: 1->1->2->3->4->4
+#
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def mergeTwoLists(self, l1, l2):
+        """
+        :type l1: ListNode
+        :type l2: ListNode
+        :rtype: ListNode
+        """
+        # brute force, iterative, compare nodes in two list one by one
+        # append to the new list
+        # O(n), 20.07%, 61ms 
+
+        ml = ListNode(0)
+        cur = ml
+
+        while (l1 or l2):
+            # a = ListNode()
+            if l2 == None:
+                # add l1 to ml
+                cur.next = l1
+                l1 = l1.next
+                cur = cur.next
+                continue
+
+            if l1 == None:
+                # add l2 to ml
+                cur.next = l2
+                l2 = l2.next
+                cur = cur.next
+                continue
+
+            if l1.val > l2.val:
+                cur.next = l2
+                l2 = l2.next
+                cur = cur.next
+            else:
+                cur.next = l1
+                l1 = l1.next
+                cur = cur.next
+
+        return ml.next
+
+        
+
+d = Solution()
+print(d.mergeTwoLists())
+
