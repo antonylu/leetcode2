7e883274d29383303c5a2be7e1e87ddf9aa76b31
Antony Lu <antony_lu@compal.com>
2018-04-30 19:14:11 +0800

    083 brute force, enumerate value of each node, 23%

---
+++ Python/083_remove-duplicates-from-sorted-list.py
@@ -0,0 +1,61 @@
+""" 
+https://leetcode.com/problems/remove-duplicates-from-sorted-list/description/
+
+Given a sorted linked list, delete all duplicates such that each element appear only once.
+
+Example 1:
+
+Input: 1->1->2
+Output: 1->2
+Example 2:
+
+Input: 1->1->2->3->3
+Output: 1->2->3
+"""
+# Definition for singly-linked list.
+class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        # Approach 1: enumerate each node, remove the same nodes in-place
+        # current: to compare, if same, check next node, if different, concatenate nodes then check next
+        # check: to be checked node
+        # 23%, 61ms
+        if head == None: return None
+        current = head
+        check = head.next
+        while current.next is not None:
+            while check is not None:
+                if current.val == check.val:
+                    check=check.next
+                    if check == None:
+                        current.next = check
+                else:
+                    current.next=check
+                    current=check
+                    check = current.next
+        return head
+        
+s = Solution()
+test_case = [[],[1,1,2],[1,1,2,3,3]]
+#test_case = [[1,1,2,3,3]]
+#test_case = [[1,1,2]]
+
+for i in test_case:
+    head = ListNode(0)
+    current = head
+    for j in i:
+        a = ListNode(j)
+        current.next = a
+        current = a
+    s.deleteDuplicates(head)
+    while head:
+        print(head.val)
+        head = head.next
