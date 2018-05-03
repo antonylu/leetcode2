bc6e655c00cdb6dc0d83d4068ed1a07e99e1e2a2
Antony Lu <antony_lu@compal.com>
2018-05-03 17:12:23 +0800

    101 DFS iteration with stack, simulate recursion. 71%

--- Python/101_symmetric-tree.py
+++ Python/101_symmetric-tree.py
@@ -34,22 +34,35 @@ class Solution(object):
         """
         # Approach 1: recursion, return False if check fail. Else return isSymmetric(left,right) and isSymmetric(right)
         # 92%, 43ms
+#        if root == None: return True
+#        return self.isSymmetricTree(root.left,root.right)
+#        
+#    def isSymmetricTree(self, p, q):
+#        """
+#        :type p: TreeNode
+#        :type q: TreeNode
+#        :rtype: bool
+#        """
+#        # Approach 3: recursive, optimize
+#        # O(n), 97%
+#        if p == None and q == None: return True
+#        if bool(p) ^ bool(q) : return False
+#        if p.val != q.val : return False
+#        return self.isSymmetricTree(p.left, q.right) and self.isSymmetricTree(p.right, q.left)
+
+        # Approach 2: use stack to do DFS recursion, return False if check fail. 
         if root == None: return True
-        return self.isSymmetricTree(root.left,root.right)
-        
-    def isSymmetricTree(self, p, q):
-        """
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: bool
-        """
-        # Approach 3: recursive, optimize
-        # O(n), 97%
-        if p == None and q == None: return True
-        if bool(p) ^ bool(q) : return False
-        if p.val != q.val : return False
-        return self.isSymmetricTree(p.left, q.right) and self.isSymmetricTree(p.right, q.left)
-        
+        s = [[root.left,root,right]]
+        while (len(s)>0):
+            [p,q] = s.pop()
+            if p == None and q == None: continue
+            if bool(p) ^ bool(q) : return False
+            if p.val != q.val : return False
+            s.append([p.left,q.right])
+            s.append([p.right,q.left])
+        return True
+
+
         
 s = Solution()
 #test_case = [[1,2,3,0,0,0],[2,5,6]]
