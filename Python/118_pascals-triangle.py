937fa372e6e91ec30caaf20ca7c6704ece359224
Antony Lu <antony_lu@compal.com>
2018-05-08 17:46:45 +0800

    118 iteration, nested for loop

--- Python/118_pascals-triangle.py
+++ Python/118_pascals-triangle.py
@@ -19,13 +19,33 @@ Output:
 ]
 """
 class Solution(object):
+    def generate2(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        # Approach #2: iteration
+        # 2 dimension array
+        # r[i][j]
+        # 95%, 33ms
+        if numRows == 0: return []
+        r = [[1]]
+        for i in range(1,numRows): #
+            rlist = [1]
+            for j in range(1,i):
+                rlist.append(r[i-1][j-1] + r[i-1][j])
+            rlist.append(1)
+
+            r.append(rlist)
+        return r
+        
+
     def generate(self, numRows):
         """
         :type numRows: int
         :rtype: List[List[int]]
         """
         # Approach #1: recursive, 
-        # Approach #2: iteration
         # based on previous list, create a new list with [0]+[1], [1]+[2]..., append 1 in front/back of the new list
         # O(n^2), 35ms, 72%
         if numRows == 0: return []
@@ -34,25 +54,30 @@ class Solution(object):
             r.append(self.pascal(i))
         return r
         
-    pas = [                 \
-                 [1],       \
-                [1,1],      \
-               [1,2,1],     \
-              [1,3,3,1],    \
-             [1,4,6,4,1]    \
-          ]
+    pas = [                                \
+                 [1],                      \
+                [1,1],                     \
+               [1,2,1],                    \
+              [1,3,3,1],                   \
+             [1,4,6,4,1],                  \
+            [1, 5, 10, 10, 5, 1],          \
+           [1, 6, 15, 20, 15, 6, 1],       \
+          [1, 7, 21, 35, 35, 21, 7, 1],    \
+         [1, 8, 28, 56, 70, 56, 28, 8, 1]  \
+        ]
     def pascal(self,n):
-        if n < 5: return self.pas[n]
+        if n < len(self.pas): return self.pas[n]
         list = self.pascal(n-1)
         rlist = [1]
         for j in range(1,len(list)):
             rlist.append(list[j-1] + list[j])
         rlist.append(1)
+        self.pas.append(rlist)
         return rlist
 
 
 if __name__ == "__main__":
     s = Solution()
-    for i in range(7):
+    for i in range(10):
         print(s.generate(i))
 
