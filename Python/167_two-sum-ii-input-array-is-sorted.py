3f11a9f0ce948add6a6385f117b9650a513a155d
Antony Lu <antony_lu@compal.com>
2018-05-11 11:16:50 +0800

    167 DP, use dict hash table to keep (target - current) value, and lookup afterward. O(n) 22%

---
+++ Python/167_two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,55 @@
+"""
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+#
+Given an array of integers that is already sorted in ascending order, find two numbers such that they add up to a specific target number.
+
+The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
+
+Note:
+
+Your returned answers (both index1 and index2) are not zero-based.
+You may assume that each input would have exactly one solution and you may not use the same element twice.
+Example:
+
+Input: numbers = [2,7,11,15], target = 9
+Output: [1,2]
+Explanation: The sum of 2 and 7 is 9. Therefore index1 = 1, index2 = 2.
+"""
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        ## Approach #1 brute force, Incremental, enumeration, two nested loops
+        ## space O(1)
+        ## time  O(n^2), beats 4%, 6637ms
+        #  
+        #for i in range(0, len(numbers)-1) :
+        #    for j in range(i+1, len(numbers)):
+        #        if (target == numbers[i]+numbers[j] ):
+        #            return [i,j]
+
+
+        # Approach #2, binary search sorted list for O(N logN)
+        
+        
+        # Approach #3
+        # no2. memorization, hash table, put (target - numbers[i]) in dict
+        # if it exists then both found. One-pass hash table
+        # hash table look up average = O(1)
+        # space O(n)
+        # time  O(n), 22%, 50 ms
+        d = {}
+        for i in range(0, len(numbers)):
+            if (numbers[i] in d.keys() ): 
+                return [d[numbers[i]]+1,i+1]
+            else:
+                d[(target- numbers[i])] = i
+
+if __name__ == "__main__":
+    s=Solution()
+    n = [2,7,11,15]
+    t = 9
+    print(s.twoSum(n,t))
