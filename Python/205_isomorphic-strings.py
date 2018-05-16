89b571cd3d0b4bf4139fca7fa6cbee981b1731bb
Antony Lu <antony_lu@compal.com>
2018-05-16 19:05:51 +0800

    205 use hash to remember mappings, replace 1st with second, compare. Switch and redo again. 66% O(n)

---
+++ Python/205_isomorphic-strings.py
@@ -0,0 +1,73 @@
+"""
+https://leetcode.com/problems/isomorphic-strings/description/
+
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character but a character may map to itself.
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+Note:
+You may assume both s and t have the same length.
+
+"""
+class Solution(object):
+    def isIsomorphic(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        # Approach #1, hash table
+        # dict[s]=t
+        # enumerate s and t, replace t with a characters
+        # compare s,t
+        # O(2n) = O(n)
+        # 66%, 57ms
+        dict = {}
+        a=s
+        b=t
+        s = list(a)
+        t = list(b)
+        sl = len(s)
+        st = len(t)
+        if sl != st: return False
+
+        for i in range(sl):
+            if s[i] in dict:
+                s[i]=dict[s[i]]
+            else: 
+                dict[s[i]] = t[i]
+                s[i]=t[i]
+        if s != t: return False 
+        
+        t = list(a)
+        s = list(b)
+        dict.clear()
+        for i in range(sl):
+            if s[i] in dict:
+                s[i]=dict[s[i]]
+            else: 
+                dict[s[i]] = t[i]
+                s[i]=t[i]
+        return s == t
+
+if __name__ == "__main__":
+    s = Solution()
+    tc  = [("egg","add"), ("foo", "bar"), ("paper","title"),("ab","aa"),("aa","ab")]
+    ans = [True, False, True, False, False]
+    for t in tc:
+        print(s.isIsomorphic(t[0],t[1]))
+
