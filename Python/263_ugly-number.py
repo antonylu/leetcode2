0a5daaee39dbc3cd3b1efcc278749ed8fa6be9ed
Antony Lu <antony_lu@compal.com>
2018-05-21 17:49:55 +0800

    263 brute force, continue /2 if %2 == 0, also 3 and 5. return (num == 1)

---
+++ Python/263_ugly-number.py
@@ -0,0 +1,54 @@
+"""
+https://leetcode.com/problems/ugly-number/description/
+
+Write a program to check whether a given number is an ugly number.
+
+Ugly numbers are positive numbers whose prime factors only include 2, 3, 5.
+
+Example 1:
+
+Input: 6
+Output: true
+Explanation: 6 = 2 × 3
+Example 2:
+
+Input: 8
+Output: true
+Explanation: 8 = 2 × 2 × 2
+Example 3:
+
+Input: 14
+Output: false 
+Explanation: 14 is not ugly since it includes another prime factor 7.
+Note:
+
+1 is typically treated as an ugly number.
+Input is within the 32-bit signed integer range: [−231,  231 − 1].
+"""
+class Solution(object):
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        # Approach #1, brute force
+        # continue /2 if %2 == 0
+        # apply 3 and 5
+        # if !=1, False
+        # 40ms, 98%
+        if num < 1: return False
+        while num%2 ==0:
+            num /=2
+        while num%3 ==0:
+            num /=3
+        while num%5 ==0:
+            num /=5
+        return num == 1
+
+
+if __name__ == "__main__":
+    tc = [5,6,8,14]
+    ans = [True, True, True, False]
+    s = Solution()
+    for t in tc:
+        print(s.isUgly(t) )
