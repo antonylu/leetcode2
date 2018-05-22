a73fcd649f9a4ed7e342dd5b9a98c9f55d58979b
Antony Lu <antony_lu@compal.com>
2018-05-22 19:27:45 +0800

    326 correct typo

--- Python/326_power-of-three.py
+++ Python/326_power-of-three.py
@@ -1,5 +1,5 @@
 """
-https://leetcode.com/problems/range-sum-query-immutable/description/
+https://leetcode.com/problems/power-of-three/description/
 
 
 Given an integer, write a function to determine if it is a power of three.
