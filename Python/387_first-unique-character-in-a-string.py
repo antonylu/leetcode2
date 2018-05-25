cb0e524748eeb6aaaeefe6947502d7306f9ccb3c
Antony Lu <antony_lu@compal.com>
2018-05-25 09:00:39 +0800

    387 add dict and collections.Counter Solution. Two loops count and search, O(n) 50%

---
+++ Python/387_first-unique-character-in-a-string.py
@@ -0,0 +1,57 @@
+"""
+https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+Given a string, find the first non-repeating character in it and return it's index. If it doesn't exist, return -1.
+
+Examples:
+
+s = "leetcode"
+return 0.
+
+s = "loveleetcode",
+return 2.
+Note: You may assume the string contain only lowercase letters.
+
+"""
+class Solution(object):
+    def firstUniqChar(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        # approach #2, use dict to save first occurences 
+        # count then find without conversion, sould be faster
+        # O(n), 50%
+        dict = {}
+        for i in range(len(s)):
+            if s[i] in dict:
+                dict[s[i]] +=1
+            else:
+                dict[s[i]] = 1
+        
+        for i in range(len(s)):
+            if dict[s[i]] == 1: return i
+        return -1
+        
+
+        # approach #1, use Counter
+        # O(n) 29%
+        from collections import Counter
+        c = Counter(s)
+        d = dict(c)
+        for i in range(len(s)):
+            if d[s[i]] == 1: return i
+        return -1
+        
+        
+        
+                
+
+if __name__ == '__main__':
+    tc = ["leetcode", "loveleetcode","aabbccddeeffgghhijkllkji","aadadaad"]
+    ans = [0,2,-1,-1]
+    s = Solution()
+    for (i) in tc:
+        print(s.firstUniqChar(i))
+
+
