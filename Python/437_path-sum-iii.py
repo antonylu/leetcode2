14e6d37f41c4ff3659d709f092fd173f58e8c695
Antony Lu <antony_lu@compal.com>
2018-05-29 15:35:12 +0800

    437 Similar to two-sum, add Solution with dict saving the pathSum before every node, so simply check nodes of (pathSum-sum) works. O(n), 93%

--- Python/437_path-sum-iii.py
+++ Python/437_path-sum-iii.py
@@ -41,6 +41,31 @@ class Solution(object):
         :type sum: int
         :rtype: int
         """
+        # Approach #2, improve with dict
+        # like two-sum question, use dict to save the number of nodes which has the same path_sum from root
+        # for every node, find the number number of paths that ends in that node and starts from previous nodes
+        # how to check? if meet, path_sum_root2end - path_sum_root2start should == target
+        # i.e., for those nodes with path_sum_root2start =  path_sum_root2end - target, they can be start nodes which made the target valid
+        # so, just look up for how many nodes with path_sum == path_sum_root2end - target is the answer
+        #
+        # expected O(n), 92%
+        self.ans = 0
+        d = {}
+        def dfs(node,path_sum):
+            d[path_sum] = d.get(path_sum,0) + 1
+            path_sum += node.val
+            self.ans += d.get(path_sum - sum, 0)
+            #print(node.val,path_sum,self.ans,d)
+            if node.left: dfs(node.left,path_sum)
+            if node.right: dfs(node.right,path_sum)
+            path_sum -= node.val
+            d[path_sum] = d.get(path_sum,0) - 1
+        
+        if not root: return 0
+        dfs(root,0)
+        return self.ans
+
+    def pathSum2(self, root, sum):
         # Approach #1, brute force
         # DFS every node and as if we take that node as starting root
         # with the every new root DFS every child node and check if the path sum == 8
@@ -68,7 +93,9 @@ class Solution(object):
 if __name__ == '__main__':
     s = Solution()
     tc = [([10,5,-3,3,2,None,11,3,-2,None,1],8),([5,3,2,3,-2,None,1],8),([],1)]
-    an = [3]
+    #tc = [([10,5,-3,3,2,None,11,3,-2,None,1],8)]
+    #tc = [([5,3,2,3,-2,None,1],8)]
+    an = [3,2,0]
     import helper
     th = helper.TestHelper()
     for t in tc:
