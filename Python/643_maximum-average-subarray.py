f33ebb133eb73f7c68c3a7e01046735422325ddf
Antony Lu <w3back@gmail.com>
2018-06-15 21:32:24 +0800

    643 sliding window, O(n), 96%

---
+++ Python/643_maximum-average-subarray.py
@@ -0,0 +1,44 @@
+"""
+https://leetcode.com/problems/maximum-average-subarray-i/description/
+
+Given an array consisting of n integers, find the contiguous subarray of given length k that has the maximum average value. And you need to output the maximum average value.
+
+Example 1:
+Input: [1,12,-5,-6,50,3], k = 4
+Output: 12.75
+
+
+Explanation: Maximum average is (12-5-6+50)/4 = 51/4 = 12.75
+
+Note:
+1 <= k <= n <= 30,000.
+Elements of the given array will be in the range [-10,000, 10,000].
+
+
+"""
+class Solution(object):
+    def findMaxAverage(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: float
+        """
+        # Approach #1, sliding window
+        # O(n), 96%
+        window = sum(nums[:k])
+        ans = window
+        for i in range(k,len(nums)):
+            window = window + nums[i] - nums[i-k]
+            ans = ans if ans > window else window
+        return ans/float(k)
+        
+
+
+if __name__ == '__main__':
+    s = Solution()
+    tc  = [ ([1,12,-5,-6,50,3], 4)]
+    ans = [ 12.75]
+    for i in range(len(tc)):
+        r = s.findMaxAverage(tc[i][0],tc[i][1])
+        print (r)
+        assert(r == ans[i])
