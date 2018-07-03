528fdc05eba00f045f25dadb40e66da633cadfc3
Antony Lu <w3back@gmail.com>
2018-07-03 09:07:20 +0800

    836 Add naive Solution, O(1), 33ms

---
+++ Python/836_rectangle-overlap.py
@@ -0,0 +1,62 @@
+"""
+https://leetcode.com/problems/rectangle-overlap/description/
+
+A rectangle is represented as a list [x1, y1, x2, y2], where (x1, y1) are the coordinates of its bottom-left corner, and (x2, y2) are the coordinates of its top-right corner.
+
+Two rectangles overlap if the area of their intersection is positive.  To be clear, two rectangles that only touch at the corner or edges do not overlap.
+
+Given two (axis-aligned) rectangles, return whether they overlap.
+
+Example 1:
+
+Input: rec1 = [0,0,2,2], rec2 = [1,1,3,3]
+Output: true
+
+Example 2:
+
+Input: rec1 = [0,0,1,1], rec2 = [1,0,2,1]
+Output: false
+
+Notes:
+
+Both rectangles rec1 and rec2 are lists of 4 integers.
+All coordinates in rectangles will be between -10^9 and 10^9.
+
+
+"""
+xrange = range
+class Solution(object):
+    def isRectangleOverlap(self, rec1, rec2):
+        """
+        :type rec1: List[int]
+        :type rec2: List[int]
+        :rtype: bool
+        """
+        # Approach #1, naive
+        #
+        # The overlap situations:
+        # 1D overlap: left1 < right2 && left2 < right1
+        # 2D overlap: left1 < right2 && left2 < right1
+        #           bottom1 < top2 && bottom2 < top1
+        # that is:
+        #  rec1[0] < rec2[2] and rec2[0] < rec1[2] and
+        #  rec1[1] < rec2[3] and rec2[1] < rec1[3]
+        #
+        #  O(1), 33ms
+
+        return rec1[0] < rec2[2] and rec2[0] < rec1[2] and rec1[1] < rec2[3] and rec2[1] < rec1[3]
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    tc =  [  ([0,0,2,2], [1,1,3,3]), ([0,0,1,1], [1,0,2,1]) ]
+    ans = [  True, False]
+    for i in range(len(tc)):
+        r = s.isRectangleOverlap(*tc[i])
+        print (r)
+        assert(r == ans[i])
